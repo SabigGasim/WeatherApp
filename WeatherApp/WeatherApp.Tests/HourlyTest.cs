1c7c6eb2d92c704bf0f2a035fa34ad23de59029b
SabigGasim <sabgasim@gmail.com>
2022-07-05 18:21:44 +0300

    refactor: WeatherApp.Tests is now under WeatherApp/WeatherApp.Tests

--- WeatherApp/WeatherApp.Tests/HourlyTest.cs
+++ WeatherApp/WeatherApp.Tests/HourlyTest.cs
@@ -1,6 +1,5 @@
 ﻿using FluentAssertions;
-using System.Collections.ObjectModel;
-using WeatherApp.Model;
+using WeatherApp.Models;
 using WeatherApp.Services;
 using WeatherApp.ViewModels;
 using Xunit;
@@ -9,14 +8,8 @@ namespace WeatherApp.Tests;
 
 public class ModelsTests
 {
-
     [Theory]
-    [InlineData(1)] [InlineData(2)] [InlineData(3)] [InlineData(4)]
-    [InlineData(5)] [InlineData(6)] [InlineData(7)] [InlineData(8)]
-    [InlineData(9)] [InlineData(10)][InlineData(11)][InlineData(12)]
-    [InlineData(13)][InlineData(14)][InlineData(15)][InlineData(16)] 
-    [InlineData(17)][InlineData(18)][InlineData(19)][InlineData(20)] 
-    [InlineData(21)][InlineData(22)][InlineData(23)][InlineData(24)]
+    [MemberData(nameof(GetData), 1, 24)]
     public async void GetHourlyListWithTimeSpacing_ShouldWork(int hours)
     {
         // Arrange
@@ -26,21 +19,29 @@ public class ModelsTests
 
         // Act 
         var list = WeatherPageVM.GetHourlyListWithTimeSpacing(hourly, hours);
-        
+
         // Assert
         var ContainsDuplicates = list.Count != list.Distinct().Count();
 
         hourly.Should().NotBeNull();
         hourly.Count.Should().Be(24);
-        
+
         list.Last().dt.Should().Be(hourly.Last().dt);
-        if(hours > 1)
+        if (hours > 1)
         {
             list.First().dt.Should().Be(hourly.First().dt);
         }
 
         list.Count.Should().Be(hours);
-        
+
         ContainsDuplicates.Should().BeFalse();
+    }
+
+    public static IEnumerable<object[]> GetData(int first, int last)
+    {
+        for(; first <= last; first++)
+        {
+            yield return new object[] { first };
+        }
     }
 }
