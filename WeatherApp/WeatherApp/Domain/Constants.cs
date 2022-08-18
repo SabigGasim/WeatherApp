ab12784d329daf394cb9907b8b5d9c5143a9d446
SabigGasim <sabgasim@gmail.com>
2022-08-18 09:54:34 +0300

    Seperate Secret Constants to ignored file

---
+++ WeatherApp/WeatherApp/Domain/Constants.cs
@@ -0,0 +1,32 @@
+﻿namespace WeatherApp.Domain
+{
+    public static partial class Constants
+    {
+        public static partial class GoogleMapsApi
+        {
+            public const string Address = "https://maps.googleapis.com/maps/";
+            public const string Autocomplete = "api/place/autocomplete";
+            public const string Details = "api/place/details";
+        }
+
+        public static partial class OpenWeatherMapApi
+        {
+            public const string Address = "https://api.openweathermap.org/data/2.5/";
+        }
+
+        public static class Symbols
+        {
+            public static class Directions
+            {
+                public const char North = '↑';
+                public const char NorthEast = '↗';
+                public const char East = '→';
+                public const char SouthEast = '↘';
+                public const char South = '↓';
+                public const char SouthWest = '↙';
+                public const char West = '←';
+                public const char NorthWest = '↖';
+            }
+        }
+    }
+}
